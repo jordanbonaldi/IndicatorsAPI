b0ce0d8fd02ecefa4c16c86bc0926d35c2d6a55a
Neferett <jordan.bonaldi@epitech.eu>
2020-03-19 00:09:10 +0000

    Implemented Alma

--- src/index.ts
+++ src/index.ts
@@ -82,6 +82,7 @@ import {
 } from "technicalindicators";
 
 import Zlema from "./indicators/Zlema";
+import Alma from "./indicators/Alma";
 
 export function reverseIndex<T>(array: T[], index: number = 0): T {
     return array[array.length - 1 - index];
@@ -92,5 +93,6 @@ export {
     TradeTypes as TradeTypes,
     EntryType as EntryType,
     Zlema,
+    Alma,
     CandleData, CandleList, sma, SMA, ema, EMA, wma, WMA, wema, WEMA, macd, MACD, rsi, RSI, bollingerbands, BollingerBands, adx, ADX, atr, ATR, truerange, TrueRange, roc, ROC, kst, KST, psar, PSAR, stochastic, Stochastic, williamsr, WilliamsR, adl, ADL, obv, OBV, trix, TRIX, forceindex, ForceIndex, cci, CCI, awesomeoscillator, AwesomeOscillator, vwap, VWAP, volumeprofile, VolumeProfile, mfi, MFI, stochasticrsi, StochasticRSI, averagegain, AverageGain, averageloss, AverageLoss, sd, SD, highest, Highest, lowest, Lowest, sum, Sum, FixedSizeLinkedList, renko, HeikinAshi, heikinashi, bullish, bearish, abandonedbaby, doji, bearishengulfingpattern, bullishengulfingpattern, darkcloudcover, downsidetasukigap, dragonflydoji, gravestonedoji, bullishharami, bearishharami, bullishharamicross, bearishharamicross, eveningdojistar, eveningstar, morningdojistar, morningstar, bullishmarubozu, bearishmarubozu, piercingline, bullishspinningtop, bearishspinningtop, threeblackcrows, threewhitesoldiers, bullishhammerstick, bearishhammerstick, bullishinvertedhammerstick, bearishinvertedhammerstick, hammerpattern, hammerpatternunconfirmed, hangingman, hangingmanunconfirmed, shootingstar, shootingstarunconfirmed, tweezertop, tweezerbottom, fibonacciretracement, ichimokucloud, IchimokuCloud, keltnerchannels, KeltnerChannels, KeltnerChannelsInput, KeltnerChannelsOutput, chandelierexit, ChandelierExit, ChandelierExitInput, ChandelierExitOutput, setConfig
 }
